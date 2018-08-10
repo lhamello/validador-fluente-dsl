bafe706f9bfa0bb326673397c821461c9c57c9b0
Luiz Henrique Araujo Mello <luiz-mello@procergs.rs.gov.br>
2018-08-10 19:44:40 -0300

    Terceira versão do validador fluente, ainda em construção.

---
+++ src/main/java/br/com/validadorfluente/novo3/DominioException.java
@@ -0,0 +1,12 @@
+package br.com.validadorfluente.novo3;
+
+import java.util.List;
+
+public class DominioException extends RuntimeException {
+
+  private static final long serialVersionUID = 1L;
+
+  public DominioException(final Class classe, final List<ErroValidacaoException> errosValidacao) {
+    // TODO Auto-generated constructor stub
+  }
+}
